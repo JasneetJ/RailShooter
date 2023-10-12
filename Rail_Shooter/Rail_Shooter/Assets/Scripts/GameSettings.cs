9191e838c28ac787d2b057a66b0ad21126de9ade
JasneetJ <1178094@edu.burnabyschools.ca>
2023-10-12 14:42:30 -0700

    save

--- Rail_Shooter/Rail_Shooter/Assets/Scripts/GameSettings.cs
+++ Rail_Shooter/Rail_Shooter/Assets/Scripts/GameSettings.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 1 = true
+    // 2 = false
+
+    [SerializeField] bool isTutorialFinished = false;
+
+    private void Awake()
     {
-        
+        if (PlayerPrefs.GetInt("isTutorialFinished") == 1)
+        {
+            isTutorialFinished = true;
+        }
+        else if (PlayerPrefs.GetInt("isTutorialFinished") == 2)
+        {
+            isTutorialFinished = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
     {
-        
+        PlayerPrefs.Save();
+        Debug.Log("quit");
     }
 }
